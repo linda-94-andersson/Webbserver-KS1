f58e4f000febf9acd0ee1938059fc54fc75dcd9f
Linda Andersson <linda.29.andersson@gmail.com>
2022-05-16 09:38:52 +0200

    Setup of exempel todos and new filename

---
+++ data.js
@@ -0,0 +1,28 @@
+const todos = [
+    {
+        id: 1,
+        title: "Coding in Javascript",
+        description: "Working with functions in JavaScript",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "Cooking dinner",
+        description: "Preparing pasta and chicken",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "Taling a walk",
+        description: "Easy time at the park",
+        completed: false,
+    },
+    {
+        id: 4,
+        title: "Wathcing Netflix",
+        description: "Enjoying the new premiered series",
+        completed: false,
+    },
+];
+
+module.exports = todos;
