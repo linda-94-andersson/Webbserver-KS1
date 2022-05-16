1b81675a89a8f03ebfab68e123155eec5c004b59
Linda Andersson <linda.29.andersson@gmail.com>
2022-05-16 09:21:19 +0200

    Simple exempel server

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const http = require("http");
 
-const PORT = process.env.PORT || 3000;
+const hostname = "127.0.0.1";
+
+const port = process.env.PORT || 5000;
+
+const server = http.createServer();
+
+server.on("request", (request, res) => {
+    res.end("Hello World!");
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
