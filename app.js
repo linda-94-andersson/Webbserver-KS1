ba44abbb562143671a4e50c72f8b2a7e1a2d36ca
Linda Andersson <linda.29.andersson@gmail.com>
2022-05-16 10:16:38 +0200

    Setup of methods for the API

--- app.js
+++ app.js
@@ -1,17 +1,56 @@
 const http = require("http");
-
-const hostname = "127.0.0.1";
+const Todo = require("./controller");
+const { getReqData } = require("./utils");
 
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(async (req, res) => {
-    if (req.url === "/api" && req.method === "GET") {
+    if (req.url === "/api/todos" && req.method === "GET") {
+        const todos = await new Todo().getTodos();
         res.writeHead(200, { "Content-Type": "application/json" });
-        res.write("This is a Vanilla Node.js API");
-        res.end();
+        res.end(JSON.stringify(todos));
+    }
+    else if (req.url.match(/\/api\/todos\/([0-9]+)/) && req.method === "GET") {
+        try {
+            const id = req.url.split("/")[3];
+            const todo = await new Todo().getTodos(id);
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(todo));
+        } catch (error) {
+            res.writeHead(404, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ message: error }));
+        }
+    }
+    else if (req.url.match(/\/api\/todos\/([0-9]+)/) && req.method === "DELETE") {
+        try {
+            const id = req.url.split("/")[3];
+            let message = await new Todo().deleteTodo(id);
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ message }));
+        } catch (error) {
+            res.writeHead(404, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ message: error }));
+        }
+    }
+    else if (req.url.match(/\/api\/todos\/([0-9]+)/) && req.method === "PATCH" || "PUT") {
+        try {
+            const id = req.url.split("/")[3];
+            let updated_todo = await new Todo().updateTodo(id);
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(updated_todo));
+        } catch (error) {
+            res.writeHead(404, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ message: error }));
+        }
+    }
+    else if (req.url === "/api/todos" && req.method === "POST") {
+        let todo_data = await getReqData(req);
+        let todo = await new Todo().createTodo(JSON.parse(todo_data));
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(todo));
     }
     else {
-        res.writeHead(400, { "Content-Type": "application/json" });
+        res.writeHead(404, { "Content-Type": "application/json" });
         res.end(JSON.stringify({ message: "Route not found" }));
     }
 });
