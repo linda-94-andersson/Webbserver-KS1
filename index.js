e0e0d54e88f036acb096de81da5193e6950dc1c9
Linda Andersson <linda.29.andersson@gmail.com>
2022-05-16 09:29:38 +0200

    New setup of server

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const http = require("http");
 
 const hostname = "127.0.0.1";
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-const server = http.createServer();
-
-server.on("request", (request, res) => {
-    res.end("Hello World!");
+const server = http.createServer(async (req, res) => {
+    if (req.url === "/api" && req.method === "GET") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.write("This is a Vanilla Node.js API");
+        res.end();
+    }
+    else {
+        res.writeHead(400, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Route not found" }));
+    }
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`);
 });
